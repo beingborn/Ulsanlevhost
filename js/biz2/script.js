5cd0ace5f1af60be048228dc6609585d3add9f0d
beingborn <dolmin0708@naver.com>
2024-12-02 09:28:25 +0900

    (20241202) 이민혁 검수전 퍼블리싱 파일 추가

---
+++ js/biz2/script.js
@@ -0,0 +1,49 @@
+$(document).ready(function () {
+  //gnb 메뉴
+  $("header .menu-area .open-btn").on("click", function () {
+    $(this).toggleClass("on");
+  });
+  $("header .menu-box .close-btn").on("click", function () {
+    $("header .open-btn").removeClass("on");
+  });
+
+  //메인 공지사항 배너
+  var mainInfoSwiper = new Swiper(".main-info-content .notice-area .swiper", {
+    loop: true,
+    autoplay: {
+      delay: 4000,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  /* 탭 변환 */
+  $('.tab-list li a').click(function () {
+    $('.tab-cont').hide().filter(this.hash).show();
+    $('.tab-list li a').removeClass('show');
+    $(this).addClass('show');
+    return false;
+  }).filter(':eq(0)').click();
+
+
+});
+
+/* 페이지 min-height 증가 */
+function adjustContainerHeight(){
+  if($('.full-page').length){
+    var headerHeight = $('header').outerHeight()
+    var footerHeight = $('footer').outerHeight()
+    var totalHeight = headerHeight + footerHeight;
+    $('.full-page .container').css('min-height', 'calc(100vh - ' + totalHeight + 'px)');
+  }
+}
+$(function(){
+  adjustContainerHeight()
+})
+$(window).on('resize', function(){
+  adjustContainerHeight()
+})
+
+
